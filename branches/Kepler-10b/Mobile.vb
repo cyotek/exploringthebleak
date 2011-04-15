fff4d35615aa77e259266d933026fbe16fd52860
nbinman <nbinman@4440b7c4-c7f5-6885-93b5-36ad71a5319c>
2011-04-15 03:20:23 +0000

    Began restructuring the entire module. About a quarter done. So far separated dimensions into classes (Item, Map, Mobile, & Player) Restructuring occuring due to plans on porting to DET-33 in anticipation of the upcoming ARRP 2011 Release. Module additions will concurrently backtrack and update code for base ETB, inheriting all implementations and code changes found helpful ; however DET-33 will fork completely after it's complete release date and will no longer be found concurrent to ETB.

---
+++ branches/Kepler-10b/Mobile.vb
@@ -0,0 +1,9 @@
+﻿Public Class Mobile
+    Public Visible(,,) As Short '3 visible settings (maxdepthlevel,maxmobiles,3)
+    Public Alive As Boolean 'death flag
+    Public Type As Short 'identifier type, or creature type
+#Region "Position"
+    Public X, Y As Short
+    Public LastMovement As Short
+#End Region
+End Class
